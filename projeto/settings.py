f17f5a92393f12220332fa713ab4f1a036d72f2d
weslley lucas <weslleysamueldj@gmail.com>
2023-05-28 18:18:59 -0400

    templates

--- projeto/settings.py
+++ projeto/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #a gente tem apps tbm
-    'recipes'
+    'recipes',
 ]
 
 MIDDLEWARE = [
--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html')
 
 def episodio(request):
     return HttpResponse('episodio')
